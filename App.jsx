6b7d98f3d366199248bc7debc82a98efc771678f
Ahmad Al-Sajid <csesajid@gmaill.com>
2016-12-14 16:41:59 +0600

    using props

--- App.jsx
+++ App.jsx
@@ -1,61 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            data : [
-                {
-                    "id" : 1,
-                    "name" : "Foo",
-                    "age" : 10
-                },
-                {
-                    "id" : 2,
-                    "name" : "Bar",
-                    "age" : 15
-                },
-                {
-                    "id" : 3,
-                    "name" : "Biz",
-                    "age" : 20
-                },
-            ]
-        }
-    }
-
     render() {
         return (
             <div>
-               <Header/>
-               <table>
-                   <tbody>
-                       { this.state.data.map((person, i) => <TableRow key ={i} data = { person } /> )}
-                   </tbody>
-               </table>
+                <h1>{this.props.headerProp}</h1>
+                <h2>{this.props.contentProp}</h2>
             </div>
-        );
-    }
-}
-
-class Header extends React.Component{
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        )
-    }
-}
-
-class TableRow extends React.Component{
-    render() {
-        return (
-            <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
         );
     }
 }
