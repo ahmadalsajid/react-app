31f4f5529e70a9a724cf32068a1dd6d3173892a6
Ahmad Al-Sajid <csesajid@gmaill.com>
2016-12-14 17:21:45 +0600

    findDOMNode(id)  method example

--- App.jsx
+++ App.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     constructor() {
         super();
-        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+        this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
     };
 
-    forceUpdateHandler() {
-        {/*Sometimes you want to update the component manually. You can
-        achieve this by using forceUpdate() method.*/}
-        this.forceUpdate();
-    };
+    findDomNodeHandler() {
+        var myDiv = document.getElementById('node2');
+        ReactDOM.findDOMNode(myDiv).style.color = 'red';
+    }
 
     render() {
         return (
             <div>
-                <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
-                <h4>Random number: {(Math.random()*100).toFixed(2)}</h4>
+                <button onClick = {this.findDomNodeHandler}>FIND NODE 2</button>
+                <div id = "myDiv">NODE</div>
+                <div id = "node2">NODE</div>
             </div>
         );
     }
